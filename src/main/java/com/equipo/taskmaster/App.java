b24203b57976b99814fda4a1f579d435cd5c83d3
Osvaldo Mella <osv.mella96@gmail.com>
2025-06-12 21:58:18 -0400

    Add: Inicializacion proyecto Jenkinsfile

---
+++ src/main/java/com/equipo/taskmaster/App.java
@@ -0,0 +1,28 @@
+package com.equipo.taskmaster;
+
+import java.util.ArrayList;
+
+public class App {
+
+    public static ArrayList<String> tasks = new ArrayList<>();
+
+    public static void addTask(String task) {
+        tasks.add(task);
+    }
+
+    public static void printTasks() {
+        System.out.println("Tareas pendientes:");
+        for (String task : tasks) {
+            System.out.println("- " + task);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a TaskMaster!");
+        System.out.println("Ambiente: " + System.getProperty("env.name"));
+        addTask("Estudiar Maven");
+        addTask("Leer Sobre CI/CD");
+        printTasks();
+    }
+
+}
---
+++ src/test/java/com/equipo/taskmaster/AppTest.java
@@ -0,0 +1,21 @@
+package com.equipo.taskmaster;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testAddTask() {
+        App.tasks.clear();
+        App.addTask("Terminar Ejercicio Maven");
+        assertEquals(1, App.tasks.size());
+    }
+}
